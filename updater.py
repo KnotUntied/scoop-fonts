de1d38f8711c64927b607a3cb1db97b9262bf941
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2021-08-12 22:03:31 +0800

    Try using Python action

---
+++ updater.py
@@ -0,0 +1,52 @@
+import hashlib
+import json
+import re
+import subprocess
+import urllib.request
+
+fonts = [
+    "ibmplexmono",
+    "ibmplexsans",
+    "ibmplexserif",
+    "recmono-casual",
+    "recmono-duotone",
+    "recmono-linear",
+    "recmono-semicasual"
+]
+
+autoupdate_re = re.compile("\$version")
+
+github_re = re.compile("\/releases\/tag\/(?:v|V)?([\d.]+)")
+
+for font in fonts:
+    with open(f"./bucket/{font}.json", "r") as f:
+        json_object = json.load(f)
+
+    if json_object["checkver"] == "github":
+        with urllib.request.urlopen(f"{json_object['homepage']}/releases") as f:
+            checkver = f.read().decode('utf-8')
+
+        checkver_version = github_re.search(checkver)
+        if (checkver_version and checkver_version.group(1)):
+            latest_version = checkver_version.group(1)
+
+        if (latest_version != json_object["version"]):
+            hashes = []
+
+            for i, url in enumerate(json_object["url"]):
+                url = autoupdate_re.sub(latest_version, json_object["autoupdate"]["url"][i])
+
+                with urllib.request.urlopen(f"{url}") as f:
+                    sha256_hash = hashlib.sha256()
+                    for byte_block in iter(lambda: f.read(4096),b""):
+                        sha256_hash.update(byte_block)
+                    hashes.append(sha256_hash.hexdigest())
+
+            json_object["version"] = latest_version
+            json_object["hash"] = hashes
+
+            with open(f"./bucket/{font}.json", "w") as f:
+                json.dump(json_object, f, indent=4)
+                f.write("\n")
+
+            subprocess.check_call(["hub", "commit", "-m", f"{font}: Update to version {latest_version}"])
