15d6710449c9f2324504cde98e12f000cf6b1a04
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2022-10-01 20:08:44 +0800

    Make pyexcavator more lenient

--- updater.py
+++ updater.py
@@ -26,32 +26,35 @@ for font in fonts:
         json_object = json.load(f)
 
     if json_object["checkver"] == "github":
-        with urllib.request.urlopen(f"{json_object['homepage']}/releases") as f:
-            checkver = f.read().decode('utf-8')
+        try:
+            with urllib.request.urlopen(f"{json_object['homepage']}/releases") as f:
+                checkver = f.read().decode('utf-8')
 
-        checkver_version = github_re.search(checkver)
-        if checkver_version and checkver_version.group(1):
-            latest_version = checkver_version.group(1)
+            checkver_version = github_re.search(checkver)
+            if checkver_version and checkver_version.group(1):
+                latest_version = checkver_version.group(1)
 
-        if latest_version != json_object["version"]:
-            hashes = []
+            if latest_version != json_object["version"]:
+                hashes = []
 
-            for i, url in enumerate(json_object["url"]):
-                if "autoupdate" in json_object:
-                    url = autoupdate_re.sub(latest_version, json_object["autoupdate"]["url"][i])
+                for i, url in enumerate(json_object["url"]):
+                    if "autoupdate" in json_object:
+                        url = autoupdate_re.sub(latest_version, json_object["autoupdate"]["url"][i])
 
-                with urllib.request.urlopen(f"{url}") as f:
-                    sha256_hash = hashlib.sha256()
-                    for byte_block in iter(lambda: f.read(4096),b""):
-                        sha256_hash.update(byte_block)
-                    hashes.append(sha256_hash.hexdigest())
+                    with urllib.request.urlopen(f"{url}") as f:
+                        sha256_hash = hashlib.sha256()
+                        for byte_block in iter(lambda: f.read(4096),b""):
+                            sha256_hash.update(byte_block)
+                        hashes.append(sha256_hash.hexdigest())
 
-            json_object["version"] = latest_version
-            json_object["hash"] = hashes
+                json_object["version"] = latest_version
+                json_object["hash"] = hashes
 
-            with open(f"./bucket/{font}.json", "w") as f:
-                json.dump(json_object, f, indent=4)
-                f.write("\n")
+                with open(f"./bucket/{font}.json", "w") as f:
+                    json.dump(json_object, f, indent=4)
+                    f.write("\n")
 
-            subprocess.check_call(["hub", "add", f"bucket/{font}.json"])
-            subprocess.check_call(["hub", "commit", "-m", f"{font}: Update to version {latest_version}"])
+                subprocess.check_call(["hub", "add", f"bucket/{font}.json"])
+                subprocess.check_call(["hub", "commit", "-m", f"{font}: Update to version {latest_version}"])
+        except Exception as e:
+            print(f"Something wrong happened with {font}.")
